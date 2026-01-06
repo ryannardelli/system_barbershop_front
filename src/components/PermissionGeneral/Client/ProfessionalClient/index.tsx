63c61d1ed6b4ec201d6c8cd2d3cf86862cadfb0a
Ryan Nardelli <ryannardelli12@gmail.com>
2026-01-06 03:04:23 -0300

    feat: Adding images of professionals

--- src/components/PermissionGeneral/Client/ProfessionalClient/index.tsx
+++ src/components/PermissionGeneral/Client/ProfessionalClient/index.tsx
@@ -1,5 +1,11 @@
+import { Heading } from "../../../Heading";
+
 export function ProfessionalClient() {
   return(
-    <h1>Profissionais</h1>
+    <section className="min-h-screen space-y-8 bg-primary text-gray-100 p-6">
+      <header className="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+        <Heading title="Nossos Profissionais" subtitle="Especialistas prontos para te atender" />
+      </header>
+    </section>
   );
 }
